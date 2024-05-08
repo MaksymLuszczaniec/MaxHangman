9feb7b381e779f7cf5ef649b32b3bcf66a300979
MaksymLuszczaniec <luszczaniec24@outlook.com>
2024-05-08 06:58:19 +0200

    Add all files and send to github

---
+++ HangmanMax/HangmanMaxtool.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanMax
+{
+    internal class HangmanMaxtool
+    {
+        public string TakeSomeWord()
+        {
+            Random r = new Random();
+            string word = "";
+
+            List<string> words = new List<string>()
+            {
+                "infamous",
+                "fabulous",
+                "puzzling",
+                "shut",
+                "interesting",
+                "grumpy",
+                "dirty",
+                "well-off",
+                "scarce",
+                "overrated",
+            };
+
+            word = words[r.Next(words.Count)]; //words[r.Next(10)];, but l want dynamic
+            // we get a random word
+            return word;
+        }
+
+        public bool ValidGuess(string guess, string lettersGuessed)
+        {
+            bool result = true; // defult to valid quess
+
+            if (guess.Length != 1) // for one char
+            {
+                Console.WriteLine("Wrong, only one letter can be choose. again");
+                result = false;
+            }
+            else if (!Char.IsLetter(guess[0]))
+            {
+                Console.WriteLine("Wrong, It's neeed to be a letter. again");
+                result = false;
+            }
+            else if (lettersGuessed.Contains(guess)) // if already choosen (letter)
+            {
+                Console.WriteLine("Wrong, you already choose that letter. again");
+                result = false;
+            }
+
+            return result;
+        }
+
+        public string UpdateWord(string playerGuessed, string rozwiazanie) 
+        {
+            string result = "";
+
+            for (int i = 0; i < rozwiazanie.Length; i++)
+            {
+                if (playerGuessed.Contains(rozwiazanie[i])) // Is a letter from list?
+                {
+                    result += rozwiazanie[i];
+                }
+                else
+                {
+                    result += "_";
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HangmanMax/Program.cs
@@ -0,0 +1,77 @@
+﻿using HangmanMax;
+
+HangmanMaxtool ht = new HangmanMaxtool();
+
+string rozwiazanie = "";
+string guessWord = "";
+string whatsLettersGuessed = "";
+int wrongGuesses = 0;
+
+bool gameIsOver = false;
+
+Console.WriteLine("Hello, play with me! Why? Becouse it's funny");
+
+Console.WriteLine("Do a world ");
+Thread.Sleep(400);
+Console.Write(".");
+Thread.Sleep(400);
+Console.Write(".");
+Thread.Sleep(400);
+Console.Write(".");
+Thread.Sleep(400);
+Console.Write(".");
+
+rozwiazanie = ht.TakeSomeWord();
+
+Console.WriteLine();
+Console.WriteLine("Ok, You want that word l think");
+
+do
+{
+    Console.WriteLine("Give me a letter please");
+
+    do
+    {
+        guessWord = Console.ReadLine();
+
+    } while (!ht.ValidGuess(guessWord, whatsLettersGuessed));
+
+    whatsLettersGuessed += guessWord;
+
+    if (rozwiazanie.Contains(guessWord))
+    {
+        Console.WriteLine($"ok, letter {guessWord} is right letter");
+
+        if (ht.UpdateWord(whatsLettersGuessed, rozwiazanie) == rozwiazanie)
+        {
+            Console.WriteLine("Omg u won, I will win next time");
+            gameIsOver = true;
+        }
+        Console.WriteLine("Word: " + ht.UpdateWord(whatsLettersGuessed, rozwiazanie));
+    }
+    else // if he is wrong
+    {
+        Console.WriteLine($"Ohh no, the word dosen't have the letter {guessWord}.");
+        wrongGuesses++;
+
+        if (wrongGuesses < 10)
+        {
+            Console.WriteLine($"You have {10 - wrongGuesses} how many try left");
+        }
+        if (wrongGuesses >=10)
+        {
+            Console.WriteLine("Ohh no, you lost, try again ahahhaha");
+            Console.WriteLine("the word is: " + rozwiazanie);
+            gameIsOver = true;
+        }
+        
+    }
+    
+
+
+} while (!gameIsOver);
+
+Console.WriteLine("Thanks for time! If u like play again");
+
+
+  
